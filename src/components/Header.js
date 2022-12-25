a2ca97be5bb03f121c18bb0cf965c1cb9d0772c6
innocentabhinav <abhinavprakashak47@gmail.com>
2022-12-26 00:52:42 +0530

    test change

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ import CloseIcon from '@mui/icons-material/Close';
 
 function Header() {
 
-  console.log('ths ist tesgsjlak');
  const [burgerStatus , setBurgerstatus] =useState(false);
 
   return (
